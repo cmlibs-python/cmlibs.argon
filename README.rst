e2646241523e462440fe08c466d8b876b29d1a5f
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-04-15 10:56:00 +1200

    Mark package as inactive.

--- README.rst
+++ README.rst
@@ -4,4 +4,7 @@
 OpenCMISS-Argon
 ===============
 
-The core data structures for GUI applications using OpenCMISS-Zinc `opencmiss.org <http://opencmiss.org>`_.
+This package has been renamed. Use :code:`pip install cmlibs.argon` instead.
+New package: https://pypi.org/project/cmlibs.argon/
+
+See `OpenCMISS2CMLibs rename tool <https://pypi.org/project/opencmiss2cmlibs/>`_ for help with renaming your own code.
--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 import io
-import os
-import re
 
-from setuptools import setup, find_packages
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, 'src', 'opencmiss', 'argon', '__init__.py')) as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
-
-if not version:
-    raise RuntimeError('Cannot find version information')
+from setuptools import setup
 
 
 def readfile(filename, split=False):
@@ -23,33 +11,17 @@ def readfile(filename, split=False):
 
 
 readme = readfile("README.rst", split=True)
-readme.append('License')
-readme.append('=======')
-readme.append('')
-readme.append('::')
-readme.append('')
-readme.append('')
 
-software_licence = readfile("LICENSE")
-
-requires = ['opencmiss.zinc', 'packaging']
+requires = ['cmlibs.argon']
 
 setup(
     name='opencmiss.argon',
-    version=version,
+    version="0.3.6",
     description='OpenCMISS-Argon visualisation descriptions.',
-    long_description='\n'.join(readme) + software_licence,
+    long_description='\n'.join(readme),
     long_description_content_type='text/x-rst',
-    classifiers=[],
-    author='Hugh Sorby',
-    author_email='h.sorby@auckland.ac.nz',
-    url='https://github.com/OpenCMISS-Bindings/opencmiss.argon',
+    classifiers=["Development Status :: 7 - Inactive"],
     license='Apache Software License',
     license_files=("LICENSE",),
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    zip_safe=False,
     install_requires=requires,
 )
-
